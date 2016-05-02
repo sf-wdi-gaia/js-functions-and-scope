d0a93867a3acac70d82477f2d9f7c09c356e092b
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-02 16:11:30 -0700

    refactor solution code

--- solution-code/independent-practice.js
+++ solution-code/independent-practice.js
@@ -1,8 +1,10 @@
-//  1. Write a function `lengths` that accepts a single parameter as an argument, namely
-// an array of strings. The function should return an array of numbers where each
-// number is the length of the corresponding string.
+/*
+1. Write a function `lengths` that accepts a single parameter as an argument, namely
+an array of strings. The function should return an array of numbers where each
+number is the length of the corresponding string.
+*/
 
-var lengths = function(arrayOfWords) {
+function lengths(arrayOfWords) {
   var lengths = [];
   for (var i = 0; i < arrayOfWords.length; i++) {
     lengths.push(arrayOfWords[i].length);
@@ -10,26 +12,27 @@ var lengths = function(arrayOfWords) {
   return lengths;
 };
 
-// 2. Write a Javascript function called `transmogrifier`
-// This function should accept three arguments, which you can assume will be numbers.
-// Your function should return the "transmogrified" result
-//
-// The transmogrified result of three numbers is the product of the first two numbers,
-// raised to the power of the third number.
+/*
+  2. Write a Javascript function called `transmogrifier`
+  This function should accept three arguments, which you can assume will be numbers.
+  The transmogrified result of three numbers is the product of the first two numbers,
+  raised to the power of the third number.
+  Your function should return the "transmogrified" result.
+*/
 
-var transmogrifier = function(num1, num2, num3) {
+function transmogrifier(num1, num2, num3) {
   var product = num1 * num2;
   var transmogrified = Math.pow(product, num3);
-  return "( " + num1 + " times " + num2 + " ) to the power of " + num3 + " is equal to " + transmogrified;
+  return transmogrified;
 };
 
-// 3.  Write a function `wordReverse` that accepts a single argument, a string. The method should return a string with the order of the words reversed. Don't worry
-// about punctuation.
+/*
+  3. Write a function `wordReverse` that accepts a single argument, a string. The method should return a string with the order of the words (not letters) reversed. Don't worry
+  about punctuation. I.e. "hello world" should become "world hello".
+*/
 
-var wordReverse = function(stringOfWords) {
+function wordReverse(stringOfWords) {
   var arrayOfWords = stringOfWords.split(' ');
   var reversedArrayOfWords = arrayOfWords.reverse();
-  var reverseStringOfWords = reversedArrayOfWords.join(' ');
-  var reverseStringOfWords = arrayOfWords.reverse().join(' ');
-  return reverseStringOfWords;
+  return reversedArrayOfWords.join(' ');
 };
